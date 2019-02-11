201ee9745687237ecd8ae920c4444e2d4f90514a
gracewgu <47403179+gracewgu@users.noreply.github.com>
2019-02-10 17:32:07 -0800

    Delete submit.sh

--- submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-simcloud job run \
-    --docker-image docker.apple.com/mdewitt/torch_jupyter_py3:latest \
-    --cpus 2 \
-    --gpus 1 \
-    --ssh \
-    --ports 8888 \
-    --timeout 1d
